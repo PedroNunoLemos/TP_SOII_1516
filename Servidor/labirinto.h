b932cddae5929976ef94386bcfde4dd21291939e
PedroNunoLemos <pedro.nuno.f.lemos@gmail.com>
2016-04-25 17:30:24 +0100

    Grrr

--- Servidor/labirinto.h
+++ Servidor/labirinto.h
@@ -1,26 +1,2 @@
 #pragma once
-#include "objectos.h"
-#include "jogadores.h"
-#include "monstros.h"
-
-typedef enum tipocelula
-{
-	PAREDE,
-	PAREDE_PO,
-	PORTA,
-	CHAO_PEDRA,
-	CHAO_PO,
-	SAIDANIVEL
-	
-} TipoCelula;
-
-typedef struct celula
-{
-	int posx;
-	int posy;
-
-	Jogador jogador;
-	Item objecto;
-	TipoCelula tipo;
-	
-} Celula;
+#include "celula.h"
