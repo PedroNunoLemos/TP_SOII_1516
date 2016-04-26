9c99f7612e091b4d14fccf0d6ad92053009a42a6
PedroNunoLemos <pedro.nuno.f.lemos@gmail.com>
2016-04-26 22:18:37 +0100

    hghjg

---
+++ Servidor/mapa.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "labirinto.h"
+
+
+
+
+int validaSalaCelula(Labirinto labirinto) {
+
+}
+
+void criaLabirinto(Labirinto labirinto) {
+
+	for (int i = 0; i < 5; i++)
+	{
+		Sala salarnd;
+
+
+
+		
+
+		}
+
+	}
+
+
+	//
+
+
+
+
+int main() { return 0; }
