b0da99ff982a7767be966d154e608641f08f96ba
Pedro Nuno Lemos <pedro.nuno.f.lemos@gmail.com>
2016-04-26 18:20:33 +0100

    Create labirinto.c

---
+++ Servidor/labirinto.c
@@ -0,0 +1,37 @@
+
+void drawlabirinto(void ){
+
+int x=0;
+int y=0;
+int rows=10;
+int columns=10;
+
+
+
+    for (x = 0; x < rows; x++) 
+    { 
+        for (y = 0; y <= columns; y++) 
+        {
+            if (x == 0 || x == rows -1)            
+            {
+                if (y == 0)  
+                printf("+");
+                else if (y == columns)
+                printf("+\n");
+                else
+                printf("-");
+            }
+        
+            else
+            {
+                if (y == 0)  
+                printf("|");
+                else if (y == columns)
+                printf("|\n");
+                else
+                printf(".");
+            }   
+        }
+
+
+}
