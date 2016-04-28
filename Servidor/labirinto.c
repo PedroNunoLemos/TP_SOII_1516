b3921d790e12a52f2fa356fcf24845bfe7af44d4
PedroNunoLemos <pedro.nuno.f.lemos@gmail.com>
2016-04-29 00:05:58 +0100

    grr

--- Servidor/labirinto.c
+++ Servidor/labirinto.c
@@ -10,42 +10,52 @@ Labirinto CriaLabirinto(Labirinto lab, int tamx, int tamy) {
 
 	int x = 0;
 	int y = 0;
+	int constroi = 0;
 
 	Labirinto tmp = lab;
 
+
 	tmp.tamx = tamx;
 	tmp.tamy = tamy;
-
 
 
 	for (x = 0; x < tamx; x++)
 	{
 		for (y = 0; y <= tamy; y++)
 		{
-
 			tmp.celula[x][y].tipo = TipoCelula_VAZIO;
 			tmp.celula[x][y].ponto.x = x;
 			tmp.celula[x][y].ponto.y = y;
 		}
 	}
 
+	for (x = 0; x < tamx; x += 10)
+	{
+		for (y = 0; y <= tamy; y += 10)
+		{
+
+			tmp = CriaSala(x, y, tmp, x);
+		}
+	}
+
 	return tmp;
 }
 
-Labirinto  CriaSala(int inix, int iniy, Labirinto lab,int seed) {
+Labirinto  CriaSala(int inix, int iniy, Labirinto lab, int seed) {
 
 	int x = 0;
 	int y = 0;
 	int tamx = 0;
 	int tamy = 0;
 
+	int prt = 0;
+
 	Labirinto tmp = lab;
 
 
 
-	tamx = aleatorio(10, 16, seed);
-	tamy = aleatorio(4, 6, seed);
-
+	tamx = aleatorio(5, 9, seed);
+	tamy = aleatorio(4, 9, seed);
 
 	for (x = 0; x < tamx; x++)
 	{
@@ -64,20 +74,38 @@ Labirinto  CriaSala(int inix, int iniy, Labirinto lab,int seed) {
 				else if (y == tamy && x == 0) novacel.tipo = TipoCelula_PAREDE_IE;
 				else if (y == 0 && x == tamx - 1) novacel.tipo = TipoCelula_PAREDE_SD;
 				else if (y == tamy && x == tamx - 1)novacel.tipo = TipoCelula_PAREDE_ID;
-				else novacel.tipo = TipoCelula_PAREDE_HZ;
+				else {
+
+					novacel.tipo = TipoCelula_PAREDE_VT;
+
+				}
 			}
 
 			else
 			{
-				if (y == 0)
-					novacel.tipo = TipoCelula_PAREDE_VT;
+				if (y == 0) {
+					novacel.tipo = TipoCelula_PAREDE_HZ;
+					int porta = aleatorio(1, 100, x);
+					if (porta < 25 && prt == 0) {
+						novacel.tipo = TipoCelula_PORTA;
+						prt = 1;
+					}
+				}
 				else if (y == tamy)
-					novacel.tipo = TipoCelula_PAREDE_VT;
+				{
+					novacel.tipo = TipoCelula_PAREDE_HZ;
+					int porta = aleatorio(1, 100, y);
+					if (porta < 25 && prt == 0) {
+						novacel.tipo = TipoCelula_VAZIO;
+						prt = 1;
+					}
+				}
 				else
 					novacel.tipo = TipoCelula_CHAO;
 			}
 
 			// fim definição tipo de celula
+
 
 			//mete no labirinto a nova celula 
 
--- Servidor/mapa.c
+++ Servidor/mapa.c
@@ -9,37 +9,54 @@
 
 int main() {
 
+	int j = 0;
 
 	//SetWindow(90, 90);
 
 	Labirinto lab;
-	lab = CriaLabirinto(lab, 80, 20);
+	lab = CriaLabirinto(lab, 80, 80);
 
-	for (int j = 0; j < 3;j++)
-	lab = CriaSala(10+j*20, j+5,lab,j);
 
-	for (int x = 0; x < lab.tamx; x++) {
-		for (int y = 0; y < lab.tamy; y++) {
 
-			if (lab.celula[x][y].tipo == TipoCelula_CHAO)
-			{
-				GoToXY(x, y);
-				printf(".");
+		for (int x = 0; x < 10; x++) {
+			for (int y = 0; y < 10; y++) {
+
+				if (lab.celula[x][y].tipo == TipoCelula_CHAO)
+				{
+					GoToXY(x, y);
+					printf(".");
+				}
+
+				else if (lab.celula[x][y].tipo == TipoCelula_VAZIO)
+				{
+					GoToXY(x, y);
+					printf(" ");
+				}
+				else if (lab.celula[x][y].tipo == TipoCelula_PORTA)
+				{
+					GoToXY( x,  y);
+					printf("D");
+				}
+
+				else if (lab.celula[x][y].tipo == TipoCelula_PAREDE_HZ)
+				{
+					GoToXY(x, y);
+					printf("-");
+				}
+
+				else if (lab.celula[x][y].tipo == TipoCelula_PAREDE_VT)
+				{
+					GoToXY(x, y);
+					printf("|");
+				}
+				else
+				{
+					GoToXY(x, y);
+					printf("#");
+				}
 			}
 
-			else if (lab.celula[x][y].tipo == TipoCelula_VAZIO)
-			{
-				GoToXY(x, y);
-				printf(" ");
-			}
-			else
-			{
-				GoToXY(x, y);
-				printf("#");
-			}
 		}
-
-	}
 
 	return 0;
 }
