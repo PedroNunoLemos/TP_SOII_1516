9b987316a455256bc352dcdebc83b21e14bf0446
Pedro Nuno Lemos <pedro.nuno.f.lemos@gmail.com>
2016-04-29 18:34:41 +0100

    Update constantes.h

--- Servidor/constantes.h
+++ Servidor/constantes.h
@@ -1,1 +1,30 @@
 #pragma once
+
+
+#define Color_Black 0
+#define Color_Blue 1
+#define Color_Green 2
+#define Color_Cyan 3
+#define Color_Red 4
+#define Color_Magenta 5
+#define Color_Yellow 6
+#define Color_White 7
+#define Color_Gray 8
+#define Color_LightBlue 9
+#define Color_LightGreen 10
+#define Color_LightCyan 11
+#define Color_LightRed 12
+#define Color_LightMagenta 13
+#define Color_LightYellow 14
+#define Color_BrightWhite 15
+
+#define key_F1 59
+#define key_UP 72
+#define key_DOWN 80
+#define key_LEFT 75
+#define key_RIGHT 77
+#define key_SPACE 32
+#define key_ENTER 13
+#define key_ESCAPE 27
+#define key_TAB 9
+#define key_INSERT 82
