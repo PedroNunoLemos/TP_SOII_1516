73ace58c1d4c61e2eb9d1b00f2f6e5d31e9be037
Pedro Nuno Lemos <pedro.nuno.f.lemos@gmail.com>
2016-04-29 18:30:41 +0100

    Update consola.h

--- Servidor/consola.h
+++ Servidor/consola.h
@@ -3,3 +3,6 @@
 
 void SetWindow(int Width, int Height);
 void GoToXY(int column, int line);
+void setcolor(WORD color);
+void setForeGroundAndBackGroundColor(int ForeGroundColor,int BackGroundColor);
+void clrbox(unsigned char x1,unsigned char y1,unsigned char x2,unsigned char y2,unsigned char bkcol);
