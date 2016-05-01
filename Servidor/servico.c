1ff0923a0cb98dd2e7313ecc0f60a9a8ab063589
PedroNunoLemos <pedro.nuno.f.lemos@gmail.com>
2016-05-01 18:00:33 +0100

    serviço nt v1

--- Servidor/servico.c
+++ Servidor/servico.c
@@ -1,186 +1,181 @@
-// MySrv.cpp : Defines the entry point for the console application.
-//
-
-#include <windows.h>
+#include <Windows.h>
 #include <stdio.h>
-#include <tchar.h>
 
-#define NOME_DO_SERVICO TEXT("ServidorDungeon")
-#define TAM 256
-
-SERVICE_STATUS          MyServiceStatus;
-SERVICE_STATUS_HANDLE   MyServiceStatusHandle;
-BOOL flag;
-
-int status;
-
-VOID  WINAPI MyServiceStart(DWORD argc, LPTSTR *argv); //Faz actualmente o vosso servidor
-VOID  WINAPI MyServiceCtrlHandler(DWORD opcode);
-
-void MostrarErro(TCHAR *str, DWORD val);
+/*
+** MACROS
+*/
+# define MY_SERVICE_NAME  TEXT("DungeonServer")
+# define MY_SERVICE_DESCRIPTOR  TEXT("Servidor Jogo DungeonSO2")
+# define MY_SERVICE_BIN_NAME  TEXT("servidor.exe")
 
 
-void _tmain()
+
+/*
+** GLOBALS
+*/
+SERVICE_STATUS g_ServiceStatus;
+SERVICE_STATUS_HANDLE g_ServiceStatusHandle;
+
+/*
+** INSTALL THE SERVICE
+*/
+BOOL InstallMyService()
 {
-	SERVICE_TABLE_ENTRY   DispatchTable[] =
-	{
-		{ NOME_DO_SERVICO, MyServiceStart },
-		{ NULL,              NULL }
-	};
 
-	if (!StartServiceCtrlDispatcher(DispatchTable))
+	SC_HANDLE schSCManager;
+	SC_HANDLE schService;
+	TCHAR szPath[MAX_PATH];
+
+	if (!GetModuleFileName(NULL, szPath, MAX_PATH))
 	{
-		MostrarErro(TEXT(" [ServidorDungeon] StartServiceCtrlDispatcher error = %d\n"), GetLastError());
+		printf("Cannot install service (%d)\n", GetLastError());
+		return (FALSE);
 	}
+
+	// Get a handle to the SCM database. 
+
+	schSCManager = OpenSCManager(
+		NULL,                    // local computer
+		NULL,                    // ServicesActive database 
+		SC_MANAGER_ALL_ACCESS);  // full access rights 
+
+	if (NULL == schSCManager)
+	{
+		printf("OpenSCManager failed (%d)\n", GetLastError());
+		return (FALSE);
+	}
+
+	// Create the service
+
+	schService = CreateService(
+		schSCManager,              // SCM database 
+		MY_SERVICE_NAME,                   // name of service 
+		MY_SERVICE_DESCRIPTOR,                   // service name to display 
+		SERVICE_ALL_ACCESS,        // desired access 
+		SERVICE_WIN32_OWN_PROCESS, // service type 
+		SERVICE_DEMAND_START,      // start type 
+		SERVICE_ERROR_NORMAL,      // error control type 
+		szPath,                    // path to service's binary 
+		NULL,                      // no load ordering group 
+		NULL,                      // no tag identifier 
+		NULL,                      // no dependencies 
+		NULL,                      // LocalSystem account 
+		NULL);                     // no password 
+
+	if (schService == NULL)
+	{
+		printf("CreateService failed (%d)\n", GetLastError());
+		CloseServiceHandle(schSCManager);
+		return (FALSE);
+	}
+	else printf("Service installed successfully\n");
+
+	CloseServiceHandle(schService);
+	CloseServiceHandle(schSCManager);
+
+	return (TRUE);
 }
 
-void WINAPI MyServiceStart(DWORD argc, LPTSTR *argv)
+/*
+** DELETE THE SERVICE
+*/
+BOOL DeleteMyService()
 {
-	HANDLE File;
-	TCHAR buffer[TAM];
-	int n1, n2;
-	DWORD nrBytes;
+	SC_HANDLE schSCManager;
+	SC_HANDLE hService;
 
-	flag = TRUE;
-
-	MyServiceStatus.dwServiceType = SERVICE_WIN32;
-	MyServiceStatus.dwCurrentState = SERVICE_START_PENDING;
-	MyServiceStatus.dwControlsAccepted = 0;
-	MyServiceStatus.dwWin32ExitCode = 0;
-	MyServiceStatus.dwServiceSpecificExitCode = 0;
-	MyServiceStatus.dwCheckPoint = 0;
-	MyServiceStatus.dwWaitHint = 0;
-
-	MyServiceStatusHandle = RegisterServiceCtrlHandler(
-		NOME_DO_SERVICO,
-		MyServiceCtrlHandler);
-
-	if (MyServiceStatusHandle == (SERVICE_STATUS_HANDLE)0)
-	{
-		MostrarErro(TEXT(" [ServidorDungeon] RegisterServiceCtrlHandler failed %d\n"), GetLastError());
-		return;
-	}
-
-	if (!SetServiceStatus(MyServiceStatusHandle, &MyServiceStatus))
-	{
-		status = GetLastError();
-		MostrarErro(TEXT(" [ServidorDungeon] SetServiceStatus error %ld\n"), status);
-		return;
-	}
-
-	//TODO: ...
-
-	// Initialization complete - report running status. 
-	MyServiceStatus.dwCurrentState = SERVICE_RUNNING;
-	MyServiceStatus.dwCheckPoint = 0;
-	MyServiceStatus.dwWaitHint = 0;
-	MyServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_PAUSE_CONTINUE;
-
-	if (!SetServiceStatus(MyServiceStatusHandle, &MyServiceStatus))
-	{
-		status = GetLastError();
-		MostrarErro(TEXT(" [ServidorDungeon] SetServiceStatus error %ld\n"), status);
-		return;
-	}
-
-	// This is where the service does its work. 
-	MostrarErro(TEXT(" [ServidorDungeon] Vou iniciar o processamento \n"), 0);
-
-	while (1) {
-
-		Sleep(1000);
+	if ((schSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS)) == NULL)
+		return (FALSE);
+	if ((hService = OpenService(schSCManager, MY_SERVICE_NAME, SERVICE_ALL_ACCESS)) == NULL)
+		return (FALSE);
+	if (!DeleteService(hService))
+		return (FALSE);
+	if (!CloseServiceHandle(hService))
+		return (FALSE);
+	return (TRUE);
 
 
-	}
-
-	MyServiceStatus.dwCurrentState = SERVICE_STOPPED;
-	MyServiceStatus.dwCheckPoint = 0;
-	MyServiceStatus.dwWaitHint = 0;
-
-	if (!SetServiceStatus(MyServiceStatusHandle, &MyServiceStatus))
-	{
-		status = GetLastError();
-		MostrarErro(TEXT(" [ServidorDungeon] SetServiceStatus error %ld\n"), status);
-	}
-
-	return;
 }
 
-VOID WINAPI MyServiceCtrlHandler(DWORD Opcode)
+/*
+** SERVICE EVENT HANDLER
+*/
+void WINAPI ServiceCtrlHandler(DWORD Opcode)
 {
 	switch (Opcode)
 	{
 	case SERVICE_CONTROL_PAUSE:
-		// Do whatever it takes to pause here. 
-		MyServiceStatus.dwCurrentState = SERVICE_PAUSED;
+		g_ServiceStatus.dwCurrentState = SERVICE_PAUSED;
 		break;
-
 	case SERVICE_CONTROL_CONTINUE:
-		// Do whatever it takes to continue here. 
-		MyServiceStatus.dwCurrentState = SERVICE_RUNNING;
+		g_ServiceStatus.dwCurrentState = SERVICE_RUNNING;
 		break;
-
 	case SERVICE_CONTROL_STOP:
-		// Do whatever it takes to stop here. 
-		MyServiceStatus.dwWin32ExitCode = 0;
-		MyServiceStatus.dwCurrentState = SERVICE_STOP_PENDING;
-		MyServiceStatus.dwCheckPoint = 0;
-		MyServiceStatus.dwWaitHint = 0;
-
-		if (!SetServiceStatus(MyServiceStatusHandle,
-			&MyServiceStatus))
-		{
-			status = GetLastError();
-			MostrarErro(TEXT(" [ServidorDungeon] SetServiceStatus error %ld\n"), status);
-		}
-		flag = FALSE;
-
-		MostrarErro(TEXT(" [ServidorDungeon] Leaving MyService \n"), 0);
-		return;
-
+		g_ServiceStatus.dwWin32ExitCode = 0;
+		g_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+		g_ServiceStatus.dwCheckPoint = 0;
+		g_ServiceStatus.dwWaitHint = 0;
+		SetServiceStatus(g_ServiceStatusHandle, &g_ServiceStatus);
+		break;
 	case SERVICE_CONTROL_INTERROGATE:
-		// Fall through to send current status. 
 		break;
-
 	default:
-		MostrarErro(TEXT(" [ServidorDungeon] Unrecognized opcode %ld\n"),
-			Opcode);
 		break;
 	}
-
-	// Send current status. 
-	if (!SetServiceStatus(MyServiceStatusHandle, &MyServiceStatus))
-	{
-		status = GetLastError();
-		MostrarErro(TEXT(" [ServidorDungeon] SetServiceStatus error %ld\n"), status);
-	}
-	return;
 }
 
-HANDLE hEventSource;
-
-void MostrarErro(TCHAR *str, DWORD val)
+/*
+** SERVICE MAIN ENTRY
+*/
+void WINAPI ServiceMain(DWORD argc, LPTSTR *argv)
 {
-	TCHAR buf[TAM * 24];
-	TCHAR *msg[2] = { buf,NULL };
 
-	_stprintf_s(buf, sizeof(buf), str, val);
 
-	if (!hEventSource) {
-		hEventSource = RegisterEventSource(NULL,            // local machine
-			NOME_DO_SERVICO); // source name
+
+	g_ServiceStatus.dwServiceType = SERVICE_WIN32;
+	g_ServiceStatus.dwCurrentState = SERVICE_START_PENDING;
+	g_ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+	g_ServiceStatus.dwWin32ExitCode = 0;
+	g_ServiceStatus.dwServiceSpecificExitCode = 0;
+	g_ServiceStatus.dwCheckPoint = 0;
+	g_ServiceStatus.dwWaitHint = 0;
+	g_ServiceStatusHandle = RegisterServiceCtrlHandler(MY_SERVICE_NAME, ServiceCtrlHandler);
+	if (g_ServiceStatusHandle == (SERVICE_STATUS_HANDLE)0)
+		return;
+
+	g_ServiceStatus.dwCurrentState = SERVICE_RUNNING;
+	g_ServiceStatus.dwCheckPoint = 0;
+	g_ServiceStatus.dwWaitHint = 0;
+	SetServiceStatus(g_ServiceStatusHandle, &g_ServiceStatus);
+
+	//do the job :P
+
+
+
+	
+
+}
+
+/*
+** MAIN ENTRY
+*/
+int main(int argc, char* argv[])
+{
+	if (argc > 1)
+	{
+		if (!strcmp(argv[1], "-i"))
+			InstallMyService();
+
+		else if (!strcmp(argv[1], "-d"))
+			if (DeleteMyService())
+				printf("\nService correctly deleted\n");
+			else
+				printf("\nService uncorrectly deleted\n");
 	}
-
-	if (hEventSource) {
-		ReportEvent(hEventSource,
-			EVENTLOG_INFORMATION_TYPE,
-			0,
-			0,
-			NULL,   // sid
-			1,
-			0,
-			(const TCHAR **)msg,
-			NULL);
+	else
+	{
+		SERVICE_TABLE_ENTRY DispatchTable[] = { { MY_SERVICE_NAME, ServiceMain },{ NULL, NULL } };
+		StartServiceCtrlDispatcher(DispatchTable);
 	}
-
+	return (EXIT_SUCCESS);
 }
