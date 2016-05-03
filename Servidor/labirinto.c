5a59e4181a74ce11d137683d3b497fe8caa32876
Pedro Nuno Lemos <pedro.nuno.f.lemos@gmail.com>
2016-05-03 12:38:14 +0100

    Update labirinto.c

--- Servidor/labirinto.c
+++ Servidor/labirinto.c
@@ -4,50 +4,51 @@
 #include "labirinto.h"
 #include "..\Controlador\uteis.h"
 
-//http://jsfiddle.net/bigbadwaffle/YeazH/
 
-Sala devolveSalaMaisPerto(Labirinto lab, Sala room) {
+//Devolve a Sala mais perto 
+Sala devolveSalaMaisPerto(Labirinto lab, Sala sala) {
 
 	int i = 0;
-	int  distance = 0;
+	int  distancia = 0;
 
-	Coordenada mid;
-	Sala closest;
+	Coordenada meio;
+	Sala maisPerto;
 
-	mid.x = room.x + (room.w / 2);
-	mid.y = room.y + (room.h / 2);
+	meio.x = sala.x + (sala.w / 2);
+	meio.y = sala.y + (sala.h / 2);
 
-	int closest_distance = 1000;
+	int maisPerto_distancia = 1000;
 
 	for (i = 0; i < 20; i++) {
 
-		Sala check = lab.salas[i];
+		Sala salatmp = lab.salas[i];
 
-		if (&check == &room) continue;
+		if (&salatmp == &sala) continue;
 
-		Coordenada check_mid;
+		Coordenada salatmp_meio;
 
-		check_mid.x = check.x + (check.w / 2);
-		check_mid.y = check.y + (check.h / 2);
+		salatmp_meio.x = salatmp.x + (salatmp.w / 2);
+		salatmp_meio.y = salatmp.y + (salatmp.h / 2);
 
 
-		distance = min(abs(mid.x - check_mid.x) - (room.w / 2) - (check.w / 2),
-			abs(mid.y - check_mid.y) - (room.h / 2) - (check.h / 2));
+		distancia = min(abs(meio.x - salatmp_meio.x) - (sala.w / 2) - (salatmp.w / 2),
+			abs(meio.y - salatmp_meio.y) - (sala.h / 2) - (salatmp.h / 2));
 
-		if (distance < closest_distance) {
+		if (distancia < maisPerto_distancia) {
 
-			closest_distance = distance;
-			closest = check;
+			maisPerto_distancia = distancia;
+			maisPerto = salatmp;
 
 		}
 
 	}
 
-	return closest;
+	return maisPerto;
 
 }
 
-int validaConflitoSala(Labirinto lab, Sala room, int ign) {
+//Valida Sala sobreposta
+int validaConflitoSala(Labirinto lab, Sala sala, int ign) {
 
 	int i = 0;
 
@@ -55,10 +56,10 @@ int validaConflitoSala(Labirinto lab, Sala room, int ign) {
 
 		if (i == ign) continue;
 
-		Sala check = lab.salas[i];
+		Sala salatmp = lab.salas[i];
 
-		if (!((room.x + room.w < check.x) || (room.x > check.x + check.w) || (room.y + room.h < check.y)
-			|| (room.y > check.y + check.h)))
+		if (!((sala.x + sala.w < salatmp.x) || (sala.x > salatmp.x + salatmp.w) || (sala.y + sala.h < salatmp.y)
+			|| (sala.y > salatmp.y + salatmp.h)))
 
 			return 1;
 
@@ -67,44 +68,158 @@ int validaConflitoSala(Labirinto lab, Sala room, int ign) {
 	return 0;
 }
 
-void validaSalas(Labirinto lab) {
+//Corrige as salas e ajusta as posicoes
+Labirinto corrigeSalas(Labirinto lab) {
 
 	int i = 0;
 	int j = 0;
-
+	
+	Labirinto tmp=lab;
+	
 	for (i = 0; i < 10; i++) {
 
-		for (j = 0; j < lab.tamsalas; j++) {
+		for (j = 0; j < tmp.tamsalas; j++) {
 
-			Sala room = lab.salas[j];
+			Sala sala = tmp.salas[j];
 
 			while (1) {
 
-				Coordenada old_position;
+				Coordenada posicaoAntiga;
 
-				old_position.x = room.x;
-				old_position.y = room.y;
+				posicaoAntiga.x = sala.x;
+				posicaoAntiga.y = sala.y;
 
-				if (room.x > 1) room.x--;
-				if (room.y > 1) room.y--;
+				if (sala.x > 1) sala.x--;
+				if (sala.y > 1) sala.y--;
 
-				if ((room.x == 1) && (room.y == 1)) break;
+				if ((sala.x == 1) && (sala.y == 1)) break;
 
-				if (validaConflitoSala(lab, room, j)) {
+				if (validaConflitoSala(tmp, sala, j)) {
 
-					room.x = old_position.x;
-					room.y = old_position.y;
+					sala.x = posicaoAntiga.x;
+					sala.y = posicaoAntiga.y;
 
 					break;
 				}
 
 			}
+			
+			tmp.salas[j]=sala;
 		}
 	}
+	
+	return tmp;
 }
 
 
+//Metodo que cria as salas no labirinto
+Labirinto CriaSalas(Labirinto lab) {
 
+	int numSalas = 20;
+	
+	int  tamMin = 5;
+	int  tamMax = 15;
+	
+	int i = 0;
+	int x = 0;
+	int y = 0;
+
+	Labirinto tmp = lab;
+
+	//Cria Salas Aleatorias e mete no labirinto
+	for (i = 0; i < numSalas; i++) {
+
+		Sala sala;
+
+		sala.x = aleatorio(1, tmp.tamx - tamMax - 1, i);
+		sala.y = aleatorio(1, tmp.tamx - tamMax - 1, i);
+
+		sala.w = aleatorio(tamMin, tamMax, i);
+		sala.h = aleatorio(tamMin, tamMax, i);
+
+		if (validaConflitoSala(tmp, sala, -1)) {
+			i--;
+			continue;
+		}
+
+		sala.w--;
+		sala.h--;
+
+		tmp.salas[i] = sala;
+
+	} //fim criação de salas
+
+	tmp=corrigeSalas(tmp); //corrige a posicao das salas geradas anteriormente
+
+	//preenchemos o chao dentro das salas
+	for (i = 0; i < numSalas; i++) {
+
+		Sala salaA = tmp.salas[i];
+
+		Sala salaB = devolveSalaMaisPerto(tmp, salaA);
+
+		Coordenada pointA;
+
+		pointA.x = aleatorio(salaA.x, salaA.x + salaA.w, 1);
+		pointA.y = aleatorio(salaA.y, salaA.y + salaA.h, 2);
+
+		Coordenada pointB;
+
+		pointB.x = aleatorio(salaB.x, salaB.x + salaB.w, 3);
+		pointB.y = aleatorio(salaB.y, salaB.y + salaB.h, 4);
+
+		while ((pointB.x != pointA.x) || (pointB.y != pointA.y)) {
+
+			if (pointB.x != pointA.x) {
+
+				if (pointB.x > pointA.x) pointB.x--;
+				else pointB.x++;
+
+			}
+			else if (pointB.y != pointA.y) {
+
+				if (pointB.y > pointA.y) pointB.y--;
+				else pointB.y++;
+
+			}
+
+			tmp.celula[pointB.x][pointB.y].tipo = TipoCelula_CHAO;
+
+		}
+	} // Fim Procesamento das  Salas
+
+
+	//Por Cada Sala coloca o chao
+	for (i = 0; i < numSalas; i++) {
+		Sala sala = tmp.salas[i];
+		for (x = sala.x; x < sala.x + sala.w; x++) {
+			for (y = sala.y; y < sala.y + sala.h; y++) {
+				tmp.celula[x][y].tipo = TipoCelula_CHAO;
+			}
+		}
+	} // Fim preenchimento
+
+
+	//Colocamos as Paredes nas Salas
+	for (x = 0; x < tmp.tamx; x++) {
+		for (y = 0; y < tmp.tamy; y++) {
+			if (tmp.celula[x][y].tipo == TipoCelula_CHAO) {
+				for (int xx = x - 1; xx <= x + 1; xx++) {
+					for (int yy = y - 1; yy <= y + 1; yy++) {
+						if (tmp.celula[xx][yy].tipo == TipoCelula_VAZIO)
+							tmp.celula[xx][yy].tipo = TipoCelula_PAREDE;
+					}
+				}
+			}
+		}
+	} // Fim de meter as Paredes Nas Salas
+
+
+
+	return tmp;
+}
+
+//Metodo principal que cria o labrinto de tamanho tamx,tamy
 Labirinto CriaLabirinto(Labirinto lab, int tamx, int tamy) {
 
 
@@ -142,107 +257,6 @@ Labirinto CriaLabirinto(Labirinto lab, int tamx, int tamy) {
 	}
 
 	tmp = CriaSalas(tmp);
-
-	return tmp;
-}
-
-Labirinto CriaSalas(Labirinto lab) {
-
-	int room_count = 20;
-	int  min_size = 5;
-	int  max_size = 15;
-	int i = 0;
-	int x = 0;
-	int y = 0;
-
-	Labirinto tmp = lab;
-
-	//Cria Salas Aleatorias e mete no labirinto
-	for (i = 0; i < room_count; i++) {
-
-		Sala room;
-
-		room.x = aleatorio(1, tmp.tamx - max_size - 1, i);
-		room.y = aleatorio(1, tmp.tamx - max_size - 1, i);
-
-		room.w = aleatorio(min_size, max_size, i);
-		room.h = aleatorio(min_size, max_size, i);
-
-		if (validaConflitoSala(tmp, room, -1)) {
-			i--;
-			continue;
-		}
-
-		room.w--;
-		room.h--;
-
-		tmp.salas[i] = room;
-
-	} //fim criação de salas
-
-	validaSalas(tmp); //compacta salas
-
-	for (i = 0; i < room_count; i++) {
-
-		Sala roomA = tmp.salas[i];
-
-		Sala roomB = devolveSalaMaisPerto(tmp, roomA);
-
-		Coordenada pointA;
-
-		pointA.x = aleatorio(roomA.x, roomA.x + roomA.w, 1);
-		pointA.y = aleatorio(roomA.y, roomA.y + roomA.h, 2);
-
-		Coordenada pointB;
-
-		pointB.x = aleatorio(roomB.x, roomB.x + roomB.w, 3);
-		pointB.y = aleatorio(roomB.y, roomB.y + roomB.h, 4);
-
-		while ((pointB.x != pointA.x) || (pointB.y != pointA.y)) {
-
-			if (pointB.x != pointA.x) {
-
-				if (pointB.x > pointA.x) pointB.x--;
-				else pointB.x++;
-
-			}
-			else if (pointB.y != pointA.y) {
-
-				if (pointB.y > pointA.y) pointB.y--;
-				else pointB.y++;
-
-			}
-
-			tmp.celula[pointB.x][pointB.y].tipo = TipoCelula_CHAO;
-
-		}
-	} // Fim Procesamento das  Salas
-
-
-	//Por Cada Sala coloca o chao
-	for (i = 0; i < room_count; i++) {
-		Sala room = tmp.salas[i];
-		for (x = room.x; x < room.x + room.w; x++) {
-			for (y = room.y; y < room.y + room.h; y++) {
-				tmp.celula[x][y].tipo = TipoCelula_CHAO;
-			}
-		}
-	} // Fim preenchimento
-
-
-	for (x = 0; x < tmp.tamx; x++) {
-		for (y = 0; y < tmp.tamy; y++) {
-			if (tmp.celula[x][y].tipo == TipoCelula_CHAO) {
-				for (int xx = x - 1; xx <= x + 1; xx++) {
-					for (int yy = y - 1; yy <= y + 1; yy++) {
-						if (tmp.celula[xx][yy].tipo == TipoCelula_VAZIO) tmp.celula[xx][yy].tipo = TipoCelula_PAREDE;
-					}
-				}
-			}
-		}
-	} // Mete Paredes Nas Salas
-
-
 
 	return tmp;
 }
