afe589b321250bf934cd09c87ac015aa14589d4b
Pedro Nuno Lemos <pedro.nuno.f.lemos@gmail.com>
2016-05-03 10:34:10 +0100

    Update monstro.h

--- Controlador/monstro.h
+++ Controlador/monstro.h
@@ -1,4 +1,23 @@
 #pragma once
+#include <windows.h>
+#include <tchar.h>
+
+#include "coordenada.h"
+
+
+#ifdef DLL_EXPORTS
+#define DLL_IMP_API __declspec(dllexport)
+#else
+#define DLL_IMP_API __declspec(dllimport)
+#endif
+
+#ifdef UNICODE
+#define tstring wstring
+#else
+#define tstring string
+#endif
+
+
 
 typedef enum tipomonstro
 {
@@ -7,11 +26,12 @@ typedef enum tipomonstro
 
 }TipoMonstro;
 
-typedef struct 
+typedef struct DLL_IMP_API _monstro
 {
 
 	int lentidao;
 	TipoMonstro tipo;
+	Coordenada posicao;
 
 	int energia;
 
