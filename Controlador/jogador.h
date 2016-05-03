75bf1bcf12a660993d0514b71756a425ca953e06
Pedro Nuno Lemos <pedro.nuno.f.lemos@gmail.com>
2016-05-03 10:32:41 +0100

    Update jogador.h

--- Controlador/jogador.h
+++ Controlador/jogador.h
@@ -1,14 +1,33 @@
 #pragma once
+#include <windows.h>
+#include <tchar.h>
 
+#include "coordenada.h"
 #include  "objecto.h"
 
-typedef struct 
+#ifdef DLL_EXPORTS
+#define DLL_IMP_API __declspec(dllexport)
+#else
+#define DLL_IMP_API __declspec(dllimport)
+#endif
+
+#ifdef UNICODE
+#define tstring wstring
+#else
+#define tstring string
+#endif
+
+
+
+
+typedef struct DLL_IMP_API _jogador
 {
 
 	 int numero;
 	 char nome[256];
 	 int lentidao;
 	 int saude;
+	 Coordenada posicao;
 
 	 Item objectos[99];
 	 
