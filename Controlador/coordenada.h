b883d5cac0eaccee3acd70c99b8a26cb2c0f3ca5
Pedro Nuno Lemos <pedro.nuno.f.lemos@gmail.com>
2016-05-03 10:31:09 +0100

    Update coordenada.h

--- Controlador/coordenada.h
+++ Controlador/coordenada.h
@@ -1,6 +1,21 @@
 #pragma once
+#include <windows.h>
+#include <tchar.h>
 
-typedef struct {
+#ifdef DLL_EXPORTS
+#define DLL_IMP_API __declspec(dllexport)
+#else
+#define DLL_IMP_API __declspec(dllimport)
+#endif
+
+#ifdef UNICODE
+#define tstring wstring
+#else
+#define tstring string
+#endif
+
+
+typedef struct DLL_IMP_API _coordenada {
 
 int x;
 int y;
