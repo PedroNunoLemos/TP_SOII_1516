9bff20c41a05e55114747eda3c24111dd67b0a54
Pedro Nuno Lemos <pedro.nuno.f.lemos@gmail.com>
2016-05-04 11:42:48 +0100

    Update servidormain.c

--- Servidor/servidormain.c
+++ Servidor/servidormain.c
@@ -27,26 +27,32 @@ DWORD dwWritten; // number of bytes written to file
 HANDLE hFile;  
 
 hFile=CreateFile("file.txt",GENERIC_READ|GENERIC_WRITE,FILE_SHARE_READ|FILE_SHARE_WRITE,0,CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL,0);
+
 if(hFile==INVALID_HANDLE_VALUE)
   {
 //    MessageBox(0,"Could not create/open a file","Error",16);
     return 0;
   }
-WriteFile(hFile,buffer,sizeof(buffer),&dwWritten,0);
+  
+//WriteFile(hFile,buffer,sizeof(buffer),&dwWritten,0);
 
     for (int y = 0; y < lab.tamx; y++) {
+    	Char tc;
             for (int  x = 0; x < lab.tamy ; x++) {
+            	
                 Celula tile = tmp.celulas[x][y];
-                if (tile == 0) this.ctx.fillStyle = '#351330'; //vazio
-                else if (tile == 1) this.ctx.fillStyle = '#64908A'; // chao
-                else this.ctx.fillStyle = '#424254'; // parede
+                if (tile==0) tc='';
+                else if (tile == 1) tc='.'; // chao
+                else tc = '#'; // parede
 
             }
+            
+            WriteFile( hFile, "Testing\n", 8, &bytes, NULL );
         }
 
 DWORD dwPtr = SetFilePointer( hFile, dwWritten, NULL, FILE_END); //set pointer position to end file
 
-WriteFile(hFile,buffer,sizeof(buffer),&dwPtr,NULL); 
+//WriteFile(hFile,buffer,sizeof(buffer),&dwPtr,NULL); 
 
 CloseHandle(hFile);
 
