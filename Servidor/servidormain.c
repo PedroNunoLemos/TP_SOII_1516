a9047183e0d847c77ce7b7467524520cd1423de1
Pedro Nuno Lemos <pedro.nuno.f.lemos@gmail.com>
2016-05-04 09:10:21 +0100

    Update servidormain.c

--- Servidor/servidormain.c
+++ Servidor/servidormain.c
@@ -22,7 +22,33 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int 
 	Labirinto lab;
 	lab = CriaLabirinto(lab, 70, 70);
 	
+char buffer[]="Write this text to file";
+DWORD dwWritten; // number of bytes written to file
+HANDLE hFile;  
 
+hFile=CreateFile("file.txt",GENERIC_READ|GENERIC_WRITE,FILE_SHARE_READ|FILE_SHARE_WRITE,0,CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL,0);
+if(hFile==INVALID_HANDLE_VALUE)
+  {
+//    MessageBox(0,"Could not create/open a file","Error",16);
+    return 0;
+  }
+WriteFile(hFile,buffer,sizeof(buffer),&dwWritten,0);
+
+    for (int y = 0; y < lab.tamx; y++) {
+            for (int  x = 0; x < lab.tamy ; x++) {
+                Celula tile = tmp.celulas[x][y];
+                if (tile == ) this.ctx.fillStyle = '#351330';
+                else if (tile == 1) this.ctx.fillStyle = '#64908A';
+                else this.ctx.fillStyle = '#424254';
+                this.ctx.fillRect(x * this.scale, y * this.scale, this.scale, this.scale);
+            }
+        }
+
+DWORD dwPtr = SetFilePointer( hFile, dwWritten, NULL, FILE_END); //set pointer position to end file
+
+WriteFile(hFile,buffer,sizeof(buffer),&dwPtr,NULL); 
+
+CloseHandle(hFile);
 
 	return 0;
 }
