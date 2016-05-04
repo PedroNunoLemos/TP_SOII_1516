3c05b88e3f70817f63ec16005c014cd4aa8a60ae
PedroNunoLemos <pedro.nuno.f.lemos@gmail.com>
2016-05-04 13:23:50 +0100

    cleanup :)

--- Servidor/servidormain.c
+++ Servidor/servidormain.c
@@ -9,16 +9,9 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int 
 
 	Labirinto lab;
 
-	char buffer[MAXTAMY + 1];
+	char buffer[MAXTAMY + 4];
 	int x = 0;
 	int y = 0;
-	
-	char tc;
-
-
-	DWORD dwWritten; // number of bytes written to file
-	HANDLE hFile;
-
 
 	HANDLE hMutex = CreateMutex(NULL, TRUE, TEXT("SERVIDORDUNGEON"));
 
@@ -32,43 +25,6 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int 
 
 	lab = CriaLabirinto(lab, 70, 70, 10);
 
-	hFile = CreateFile("file.txt", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
-
-	if (hFile == INVALID_HANDLE_VALUE)
-	{
-		    MessageBox(0,"Could not create/open a file","Error",16);
-		return 0;
-	}
-
-	//WriteFile(hFile,buffer,sizeof(buffer),&dwWritten,0);
-
-	for (y = 0; y < lab.tamx; y++) {
-
-		
-		for (x = 0; x < lab.tamy; x++) {
-
-			tc = '*';
-
-			if (x == 0) buffer[0] = '\0';
-
-			Celula tile = lab.celula[x][y];
-			if (tile.tipo == TipoCelula_VAZIO) tc = (char)(256);
-			else if (tile.tipo == TipoCelula_CHAO) tc = '.'; // chao
-			else tc = '#'; // parede
-
-			buffer[x] = tc;
-		}
-
-		buffer[x + 1] = '\0';
-		WriteFile(hFile, buffer, sizeof(buffer), &dwWritten, 0);
-
-	}
-
-	DWORD dwPtr = SetFilePointer(hFile, dwWritten, NULL, FILE_END); //set pointer position to end file
-
-	//WriteFile(hFile,buffer,sizeof(buffer),&dwPtr,NULL); 
-
-	CloseHandle(hFile);
 
 	return 0;
 }
