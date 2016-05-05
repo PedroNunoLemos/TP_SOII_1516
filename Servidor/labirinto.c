022398fb504642d49a47ddc25aec47e341b81e46
Pedro Nuno Lemos <pedro.nuno.f.lemos@gmail.com>
2016-05-05 15:53:13 +0100

    Update labirinto.c

--- Servidor/labirinto.c
+++ Servidor/labirinto.c
@@ -10,7 +10,9 @@ void CriaLabirinto(Labirinto *lab, int tamx, int tamy, int salas){
 	int tamsalaw=10;
 	int tamsal=10;
 	int i=0;
+	int r = 0;
 	int ok;
+	
 	
 	lab=malloc(sizeof(Labirinto));
 	lab->tamx=tamx;
@@ -26,14 +28,35 @@ void CriaLabirinto(Labirinto *lab, int tamx, int tamy, int salas){
     		{
     			Sala *sala=malloc(sizeof(Sala));
     			
-    			Sala->x=floor((aleatorio(1,100,i)/100)*lab->tamx);
-			Sala->y=floor((aleatorio(1,100,i+1)/100)*lab->tamy);
+    			sala->x=floor((aleatorio(1,100,i)/100)*lab->tamx);
+			sala->y=floor((aleatorio(1,100,i+1)/100)*lab->tamy);
 			
-			Sala->w=floor((aleatorio(1,tamsalaw,i+2)/100)*5);
-			Sala->h=floor((aleatorio(1,tamsalah,i+3)/100)*5);
+			sala->w=floor((aleatorio(1,tamsalaw,i+2)/100)*5);
+			sala->h=floor((aleatorio(1,tamsalah,i+3)/100)*5);
+			 
+			 // check bounds    
+        		if(sala.x + sala.w >= width || sala.y + sala.h >= height){
+                		continue;
+        		}
+        		
+        		        // check against other rooms
+        
+        	for(r = 0; r < salas ; r++){
+        		
+            	if(sala->x > lab->salas[r].x && 
+                	sala->x < lab->salas[r]->x + sala->w && 
+                	sala->y > lab->salas[r]->y && 
+                	sala->y < lab->salas[r]->y + sala->h)
+                	{
+                		ok = 0;
+                		break;
+            		}
+        	}
+		
+		        ok = 1;
+		        lab->salas[]=&sala;	
 			
-			
-		}
+		}//fim while
 		
 	
 	}	
