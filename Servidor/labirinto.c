7f5d7251ae2f3b210e6e12f8554a4e311d4b55fe
Pedro Nuno Lemos <pedro.nuno.f.lemos@gmail.com>
2016-05-05 16:06:48 +0100

    Update labirinto.c

--- Servidor/labirinto.c
+++ Servidor/labirinto.c
@@ -13,6 +13,7 @@ void CriaLabirinto(Labirinto *lab, int tamx, int tamy, int salas){
 	int r = 0;
 	int ok;
 	
+	Sala *salasLigadas;
 	
 	lab=malloc(sizeof(Labirinto));
 	lab->tamx=tamx;
@@ -20,10 +21,11 @@ void CriaLabirinto(Labirinto *lab, int tamx, int tamy, int salas){
 	
 	lab->tamsalas=salas;
 	
-	
+	// cria salas no labirinto
 	for (i = 0 ; i < salas; i++){
    		ok= false;
    		
+   		//cria sala aleatoria e procura espaço vazio no labirinto para colocar a sala
    		while (!ok)
     		{
     			Sala *sala=malloc(sizeof(Sala));
@@ -39,7 +41,7 @@ void CriaLabirinto(Labirinto *lab, int tamx, int tamy, int salas){
                 		continue;
         		}
         		
-        		        // check against other rooms
+        	// valida outras salas no labirinto
         
         	for(r = 0; r < salas ; r++){
         		
@@ -54,11 +56,70 @@ void CriaLabirinto(Labirinto *lab, int tamx, int tamy, int salas){
         	}
 		
 		        ok = 1;
-		        lab->salas[]=&sala;	
+		        lab->salas[i]=&sala;	
 			
-		}//fim while
+	    } //fim criação e colocação de sala
 		
 	
-	}	
+	} // fim criação de salas	
+
+	//liga  as salas com corredores e coloca no labirinto
+	
+	
+	for (i = 0; i < connectionCount; i++)
+	{
+   		var roomA = roomList[i],
+       		roomNum = i;
+    
+       		while(roomNum == i){
+           		roomNum = ~~(Math.random()*roomList.length);
+       		}
+    
+       		roomB = roomList[roomNum];
+    
+   	//Increasing this number will make hallways straighter
+   	//Decreasing this number will make halways skewer
+   	var sidestepChance = 10,
+       		pointA = {x : ~~(Math.random()*roomA.w) + roomA.x,
+                	 y : ~~(Math.random()*roomA.h) + roomA.y},
+                	 
+       		pointB = {x : ~~(Math.random()*roomB.w) + roomB.x,
+       			y : ~~(Math.random()*roomB.h) + roomB.y};
+
+   	//This is a type of drunken/lazy walk algorithm    
+    		
+    		while (pointB.x !== pointA.x || pointB.y !== pointA.y){
+        	
+        	var num = Math.random()*100;
+      
+        	if (num < sidestepChance){
+        		
+            		if (pointB.x !== pointA.x){
+            			
+                		if(pointB.x > pointA.x){
+                			pointB.x--;
+                		}else{
+                			pointB.x++;
+                		}
+            			
+            		}
+        		
+        	}else if(pointB.y !== pointA.y){
+        		
+        		if(pointB.y > pointA.y){
+        			pointB.y--;
+        		}else{
+        			pointB.y++;
+        		}
+        		
+        	}
+        
+		if(pointB.x < width && pointB.y < height){
+            		connectedCells.push({x:pointB.x, y:pointB.y});
+			
+		}
+    	}// fim lazy walk
+	} // Fim ligação de Salas
+
 	
 }
