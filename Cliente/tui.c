6254426ae4ec8244e4b6593eb81dfc94c2eb3c21
Pedro Nuno Lemos <pedro.nuno.f.lemos@gmail.com>
2016-05-12 16:28:08 +0100

    Update tui.c

--- Cliente/tui.c
+++ Cliente/tui.c
@@ -1,35 +1,71 @@
 #include "tui.h"
 
-void imprimeLabirinto(int x, int y, int mx, int my,  Jogo lab) {
+
+int validaMovimento(Labirinto lab, int x, int y) {
+
+
+	
+
+	if ((
+		lab.celula[x][y].tipo == TipoCelula_CHAO ||
+		lab.celula[x][y].tipo == TipoCelula_PORTA))
+		return 1; else return 0;
+
+
+}
+
+void imprimeLabirinto(int x, int y, int mx, int my, int visx, int visy, Labirinto tmp) {
 
 
 	int ix = 0;
 	int iy = 0;
+	int px;
+	int py;
 
-	GoToXY(x, y);
+	int cx = 0;
+	int cy = 0;
 
-	if (mx <= 0)mx = 0;
-	if (my <= 0)my = 0;
+	Labirinto lab = tmp;
 
-	if (mx >= lab.tam)mx = lab.tam;
-	if (my >= lab.tam)my = lab.tam;
 
-	for (ix = 0; ix < lab.tam; ix++)
+	px = x + (visx / 2);
+	py = y + (visy / 2);
+
+	for (ix = 0; ix < tmp.tamx; ix++)
 	{
-		for (iy = 0; iy < lab.tam; iy++) {
+		for (iy = 0; iy < tmp.tamy; iy++) {
 
-			GoToXY(x + ix, y + iy);
+			if ((ix >= mx - (visx / 2) && iy >= my - (visy / 2))
+				&& (ix <= mx + (visx / 2) && iy <= my + (visy / 2))
+				)
+			{
 
-			if (lab.mapaAtual[mx + ix][my + iy].tipo == TipoCelula_CHAO) printf(".");
-			else if (lab.mapaAtual[mx + ix][my + iy].tipo == TipoCelula_PAREDE) printf("#");
-			else if (lab.mapaAtual[mx + ix][my + iy].tipo == TipoCelula_VAZIO) printf(" ");
-			else printf(" ");
+				GoToXY(x + (ix - mx + (visx / 2)), y + (iy - my + (visy / 2)));
 
+
+				if (lab.celula[ix][iy].tipo == TipoCelula_CHAO
+					|| lab.celula[ix][iy].tipo == TipoCelula_PORTA)
+				{
+					setForeGroundAndBackGroundColor(0, 6); printf(".");
+				}
+
+				else if (lab.celula[ix][iy].tipo == TipoCelula_PAREDE) { setcolor(Color_White); printf("*"); }
+				else if (lab.celula[ix][iy].tipo == TipoCelula_VAZIO) { setcolor(Color_Gray); printf(","); }
+				else { setcolor(Color_Black); printf(" "); }
+
+
+			}
 		}
 
 	}
 
 
+	GoToXY(px, py);
+	setcolor(Color_White);
 
+	printf("@");
+
+	GoToXY(0, 0);
+	printf("%d %d %d ", ix, mx + visx, my + visy);
 
 }
